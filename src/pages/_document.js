cf92d8462a4b3337e4c706956b34a05428aa26ba
窪山天望 <shiggyjr1492@gmail.com>
2023-10-25 14:26:18 +0900

    fix: 余計な文字列を削除しました。

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head>
-        <meta name="google-site-verification" content="NR7ckILI_D-jmfo6nO6RceBVbruKojRD7UwaHxNb5Xc" />Ï
+        <meta name="google-site-verification" content="NR7ckILI_D-jmfo6nO6RceBVbruKojRD7UwaHxNb5Xc" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       </Head >
       <body>
